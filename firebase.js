bf96c2c4fe5d7b8018b9f31d9b2def5e9ef9bc58
IrfanMohammed-890 <imohdirfanrr2022@gmail.com>
2025-05-12 17:17:18 +1000

    Update packages for application and firebase credentials

--- firebase.js
+++ firebase.js
@@ -1,31 +1,9 @@
-// firebase.js
-// import { initializeApp } from 'firebase/app';
-// import { getAuth } from 'firebase/auth';
-// import { getFirestore } from 'firebase/firestore';
-
-// const firebaseConfig = {
-//   apiKey: 'YOUR_API_KEY',
-//   authDomain: 'your-app.firebaseapp.com',
-//   projectId: "safespace-2eee7",
-//   storageBucket: 'your-app.appspot.com',
-//   messagingSenderId: 'YOUR_MSG_SENDER_ID',
-//   appId: 'YOUR_APP_ID',
-// };
-
-// const app = initializeApp(firebaseConfig);
-
-
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAZ-lmkPJC6XkV3ZGbrV-dAPrWwrmwb47M",
   authDomain: "safespace-2eee7.firebaseapp.com",
@@ -38,6 +16,14 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// export const analytics = getAnalytics(app);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+
+// Initialize Firebase Auth with AsyncStorage persistence
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+// Export initialized services
+export { app, auth, db };
